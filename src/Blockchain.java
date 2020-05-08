a38071791d7517455e1196e484b56e9f73dc5324
Alaa Shehab <alaa.shehab.95@gmail.com>
2020-05-08 19:42:31 +0200

    Architecture-modification

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -3,7 +3,8 @@ import java.util.List;
 
 public class Blockchain {
     public Block head;
-    public List<Block> chain = new LinkedList<>();
+    //Block chain is tree not just a list
+    public List<Blockchain> chain = new LinkedList<>();
 
     public Blockchain () {
         //TODO create gensis block
